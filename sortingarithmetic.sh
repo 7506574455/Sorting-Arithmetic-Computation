cc52d524a15dd29e68779c3c7f6adef48de15c3d
7506574455 <sanketlalge09@gmail.com>
2021-06-04 06:15:00 -0700

    Result in Dictionary for computation

--- sortingarithmetic.sh
+++ sortingarithmetic.sh
@@ -1,11 +1,16 @@
-read -p "Enter 1st number: " a
-read -p "Enter 2nd number: " b
-read -p "Enter 3rd number: " c
-arithmeticOp1=$((a+(b*c)))
-arithmeticOp2=$(((a*b)+c))
-arithmeticOp3=$((c+(a/b)))
-arithmeticOp4=$(((a%b)+c))
-echo "ArithmeticOp1= $arithmeticOp1"
-echo "ArithmeticOp2= $arithmeticOp2"
-echo "ArithmeticOp3= $arithmeticOp3"
-echo "ArithmeticOp4= $arithmeticOp4"
+echo "Sorting Arithmetic Computation Problem"
+
+read -p "Enter The value for a : " a
+read -p "Enter The value for b : " b
+read -p "Enter The value for c : " c
+
+declare -A dict
+compute1=$((a+b*c))
+compute2=$((a*b+c))
+compute3=$((c+a/b))
+compute4=$((a%b+c))
+dict[0]=$compute1
+dict[1]=$compute2
+dict[2]=$compute3
+dict[3]=$compute4
+echo "The Computations are: ${dict[@]}"
